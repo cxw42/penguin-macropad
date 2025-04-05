7303bdfc989b149f80266c0397451fa214f171f5
Chris White <cxwembedded@gmail.com>
2025-04-05 15:08:26 -0400

    Clean up code.py

--- code.py
+++ code.py
@@ -1,47 +1,21 @@
-# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+# 3x3 keypad.  Based on code by Kattni Rembor and Dan Halbert of Adafruit Industries.
+# By cxw42 2025
+# Matrix scanning: <https://learn.adafruit.com/key-pad-matrix-scanning-in-circuitpython/keymatrix>
+# HID: <https://learn.adafruit.com/circuitpython-essentials/circuitpython-hid-keyboard-and-mouse>
+#
+# Requires the adafruit_hid library, <https://docs.circuitpython.org/projects/hid/en/latest/index.html>
 #
 # SPDX-License-Identifier: MIT
 
-"""
-Blink example for boards with ONLY a NeoPixel LED (e.g. without a built-in red LED).
-Includes QT Py and various Trinkeys.
-
-Requires two libraries from the Adafruit CircuitPython Library Bundle.
-Download the bundle from circuitpython.org/libraries and copy the
-following files to your CIRCUITPY/lib folder:
-* neopixel.mpy
-* adafruit_pixelbuf.mpy
-
-Once the libraries are copied, save this file as code.py to your CIRCUITPY
-drive to run it.
-"""
 import time
 import board
-import neopixel
 
+import keypad
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
-print("Hello, world!")
-"""
-while True:
-    pixels.fill((255, 255, 0))
-    time.sleep(0.5)
-    pixels.fill((0, 255, 255))
-    time.sleep(0.5)
-    pixels.fill((255, 0, 255))
-    time.sleep(0.5)
-"""
-#pixels.fill((255,0,0))
-#while True: pass
-
-import keypad
-import board
-
-keyboard = Keyboard(usb_hid.devices)
-
+# Keymap.  I had to split row 1 into two row pins because of space constraints.
 key_number_to_keycode = [
     None,
     Keycode.B,    # R1C2
@@ -57,26 +31,39 @@ key_number_to_keycode = [
     Keycode.I,    # R3C3
 ]
 
-km = keypad.KeyMatrix(
-    row_pins=(board.D9, board.D1, board.D4, board.A3),
-    column_pins=(board.A2, board.A1, board.A0),
-    columns_to_anodes=True,
-)
+def main():
+    print("Hello, world!")
 
-while True:
-    event = km.events.get()
-    if not event:
-        continue
+	# Initialize
+    km = keypad.KeyMatrix(
+        row_pins=(board.D9, board.D1, board.D4, board.A3),
+        column_pins=(board.A2, board.A1, board.A0),
+        columns_to_anodes=True,
+    )
 
-    keycode = key_number_to_keycode[event.key_number]
-    print(keycode, event.pressed)
-    try:
-        if event.pressed:
-            keyboard.press(keycode)
-        else:
-            keyboard.release(keycode)
-    except ValueError:
-        # > 6 keys concurrently
-        keyboard.release_all()
+    keyboard = Keyboard(usb_hid.devices)
 
-    time.sleep(0.005)    # ~200 Hz
+    while True:
+        event = km.events.get()
+        if not event:
+            continue
+
+        keycode = key_number_to_keycode[event.key_number]
+        if not keycode:
+            continue
+
+        # Handle the event
+        print(keycode, event.pressed)
+        try:
+            if event.pressed:
+                keyboard.press(keycode)
+            else:
+                keyboard.release(keycode)
+        except ValueError:
+            # > 6 keys concurrently
+            keyboard.release_all()
+
+        time.sleep(0.005)    # ~200 Hz
+
+if __name__ == '__main__':
+    main()
